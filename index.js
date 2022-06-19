6adf826c68786fdd149b50b75f2a130f909c0bab
Hans Reichgelt <hreichgelt06@gmail.com>
2022-06-19 12:37:12 -0400

    separate question in indexjs for different titles

--- index.js
+++ index.js
@@ -9,38 +9,50 @@ const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 const Manager = require('./lib/manager');
 
-// array of questions for team member input 
+// array of questions for team member input - need to break up for individual
 const questions = [
+    {
+        type: 'list',
+        name: 'title',
+        message: 'What is the employees title?',
+        choices: ['Engineer', 'Intern', 'Manager']
+    },
     {
         type: 'input',
         name: 'name',
-        message: 'What is your name?'
+        message: 'What is the employees name?'
     },
     {
         type: 'input',
         name: 'id',
-        message: 'What is your ID Number?'
+        message: 'What is the employees ID Number?'
     },
     {
         type: 'input',
         name: 'email',
-        message: 'What is your email address?'
+        message: 'What is the employees email address?'
     },
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is your title?'
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'What is your Github username?'
-    },
+
+    // question for manager
     {
         type: 'input',
         name: 'office',
         message: 'What is your office number?'
     },
+    // question for engineer
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your Github username?'
+    },
+    // question for intern
+        // question for manager
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What school did/do you attend?'
+        },
+
 ];
 
 // create a for loop to run through questions for each staff member?
@@ -49,7 +61,7 @@ const questions = [
 // create function to write HTML File
 function writeToFile(filename, data) {
     let content = generateHTML(data);
-    fs.writeFile('./output/index.html', content, function (error){
+    fs.writeFile('./output/index.html', content, function (error) {
         if (error) {
             return console.log(error)
         }

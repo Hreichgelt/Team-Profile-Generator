8118d404aa36ec27c6d2765a8cdce44dfa291a9b
Hans Reichgelt <hreichgelt06@gmail.com>
2022-06-20 14:17:36 -0400

    idk

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 // include packages required for application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const path = require('path');
+const output = path.resolve(__dirname, 'output');
+const outputFile = path.join(output, 'index.html')
 
 // Exports
 const generateHTML = require("./utils/generateHTML.js");
 const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
 const Manager = require("./lib/manager");
-let employeesArr = [];
+
+employeesArr = [];
+
+function init () {
+    function teamCreate () {
+        inquirer.prompt([{
+            type: "list",
+            name: "title",
+            message: "What is the employees title?",
+            choices: ["Engineer", "Intern", "Manager"],
+        }]).then(function (response) {
+            switch(response.title) {
+                case 'manager': addManager();
+                break;
+                case 'engineer': addEngineer();
+                break;
+                case 'intern': addIntern();
+
+                default: builtHTML();
+            }
+        })
+    }
+}
 
 // array of questions for team member input
 const questions = [
@@ -59,3 +84,25 @@ internQuest = [
   },
 ];
 
+addManager(questions, managerQuest).then(answers.questions, answers.managerQuest);
+employeesArr.push(Manager);
+teamCreate();
+
+addEngineer(questions, engineerQuest).then(answers.questions, answers.engineerQuest);
+employeesArr.push(Engineer);
+teamCreate();
+
+addIntern(questions,internQuest).then(answers.questions, answers.internQuest);
+employeesArr.push(Intern);
+teamCreate();
+
+function builtHTML() {
+    console.log('Your html has been created')
+
+    fs.writeFileSync(outputFile, generateHTML(employeesArr))
+
+}
+
+teamCreate();
+
+init();

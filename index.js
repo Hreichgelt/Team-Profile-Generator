39523c56c6f9343f23c2263542cbc0a7e90cdfcb
Hans Reichgelt <hreichgelt06@gmail.com>
2022-06-20 14:46:19 -0400

    trying a new way

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // include packages required for application
 const inquirer = require("inquirer");
 const fs = require("fs");
-const path = require('path');
-const output = path.resolve(__dirname, 'output');
-const outputFile = path.join(output, 'index.html')
+const path = require("path");
+const output = path.resolve(__dirname, "output");
+const outputFile = path.join(output, "index.html");
 
 // Exports
 const generateHTML = require("./utils/generateHTML.js");
@@ -13,96 +13,148 @@ const Manager = require("./lib/manager");
 
 employeesArr = [];
 
-function init () {
-    function teamCreate () {
-        inquirer.prompt([{
-            type: "list",
-            name: "title",
-            message: "What is the employees title?",
-            choices: ["Engineer", "Intern", "Manager"],
+function init() {
+  function teamCreate() {
+    inquirer.prompt([{
+          type: "list",
+          name: "title",
+          message: "What is the employees title?",
+          choices: ["Engineer", "Intern", "Manager"],
         }]).then(function (response) {
-            switch(response.title) {
-                case 'manager': addManager();
-                break;
-                case 'engineer': addEngineer();
-                break;
-                case 'intern': addIntern();
+        switch (response.title) {
+          case "manager":
+            addManager();
+            break;
+          case "engineer":
+            addEngineer();
+            break;
+          case "intern":
+            addIntern();
 
-                default: builtHTML();
-            }
-        })
-    }
+          default:
+            builtHTML();
+        }
+      });
+  }
 }
 
-// array of questions for team member input
-const questions = [
-  {
-    type: "list",
-    name: "title",
-    message: "What is the employees title?",
-    choices: ["Engineer", "Intern", "Manager"],
-  },
-  {
-    type: "input",
-    name: "name",
-    message: "What is the employees name?",
-  },
-  {
-    type: "input",
-    name: "id",
-    message: "What is the employees ID Number?",
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "What is the employees email address?",
-  },
-];
+function addManager() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the employees name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the employees ID Number?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the employees email address?",
+      },
+      {
+        type: "input",
+        name: "office",
+        message: "What is your office number?",
+      },
+    ])
+    .then((response) => {
+      const manager = new Manager(
+        response.title,
+        response.name,
+        response.id,
+        response.email,
+        response.office
+      );
+      employeesArr.push(manager);
+      teamCreate();
+    });
+}
 
-// question for manager
-managerQuest = [
-  {
-    type: "input",
-    name: "office",
-    message: "What is your office number?",
-  },
-];
-// question for engineer
-engineerQuest = [
-  {
-    type: "input",
-    name: "github",
-    message: "What is your Github username?",
-  },
-];
-// question for intern
-internQuest = [
-  {
-    type: "input",
-    name: "school",
-    message: "What school did/do you attend?",
-  },
-];
+function addEngineer() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the employees name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the employees ID Number?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the employees email address?",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is your Github username?",
+      },
+    ])
+    .then((response) => {
+      const engineer = new Engineer(
+        response.title,
+        response.name,
+        response.id,
+        response.email,
+        response.github
+      );
+      employeesArr.push(engineer);
+      teamCreate();
+    });
+}
 
-addManager(questions, managerQuest).then(answers.questions, answers.managerQuest);
-employeesArr.push(Manager);
-teamCreate();
-
-addEngineer(questions, engineerQuest).then(answers.questions, answers.engineerQuest);
-employeesArr.push(Engineer);
-teamCreate();
-
-addIntern(questions,internQuest).then(answers.questions, answers.internQuest);
-employeesArr.push(Intern);
-teamCreate();
+function addIntern() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the employees name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the employees ID Number?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the employees email address?",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "What school did/do you attend?",
+      },
+    ])
+    .then((response) => {
+      const intern = new Intern(
+        response.title,
+        response.name,
+        response.id,
+        response.email,
+        response.school
+      );
+      employeesArr.push(intern);
+      teamCreate();
+    });
+}
 
 function builtHTML() {
-    console.log('Your html has been created')
+  console.log("Your html has been created");
 
-    fs.writeFileSync(outputFile, generateHTML(employeesArr))
-
+  fs.writeFileSync(outputFile, generateHTML(employeesArr));
 }
 
-teamCreate();
+// teamCreate();
 
 init();

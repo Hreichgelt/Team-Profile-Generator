1a4cd0c23a16cb374352ee7150756b530ef7272d
Hans Reichgelt <hreichgelt06@gmail.com>
2022-06-21 09:00:43 -0400

    adding links to emails and github, fixing styling issues and adding icons

--- utils/generateHTML.js
+++ utils/generateHTML.js
@@ -9,8 +9,9 @@ function generateHTML(data) {
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+      integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous" />
       <link rel="stylesheet" href="style.css">
-      <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
   
       <title>My Team | Info</title>
     </head>
@@ -54,49 +55,55 @@ function generateEmployeeCards(employeeArr) {
 
 function generateManagerCard(manager) {
   return `
-      <div class="card" style="width: 18rem">
-      <div class="card-body">
-        <h5 class="card-title">${manager.name}</h5>
-        <h6 class="card-subtitle mb-2 text-muted">${manager.title}</h6>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item id">Employee ID: ${manager.id}</li>
-          <li class="list-group-item office">Office number: ${manager.office}</li>
-          <li class="list-group-item email">Employee Email: ${manager.email}</li>
-        </ul>
-      </div>
+      <div class="card employee-card" style="width: 18rem">
+        <div class="card-header bg-primary text-light">
+          <h2 class="card-title">${manager.name}</h2>
+          <h3 class="card-subtitle"><i class="fas fa-mug-hot mr-2"></i>${manager.title}</h3>
+        </div>
+        <div class="card-body"
+          <ul class="list-group">
+            <li class="list-group-item id">Employee ID: ${manager.id}</li>
+            <li class="list-group-item office">Office number: ${manager.office}</li>
+            <li class="list-group-item email">Employee Email: <a href="mailto:${manager.email}">${manager.email}</a></li>
+          </ul>
+        </div>
       </div>
 `;
 }
 
 function generateEngineerCard(engineer) {
   return `
-      <div class="card" style="width: 18rem">
-      <div class="card-body">
-        <h5 class="card-title">${engineer.name}</h5>
-        <h6 class="card-subtitle mb-2 text-muted">${engineer.title}</h6>
-        <ul class="list-group list-group-flush">
+    <div class="card employee-card" style="width: 18rem">
+      <div class="card-header bg-primary text-light">
+        <h2 class="card-title">${engineer.name}</h2>
+        <h3 class="card-subtitle"><i class="fas fa-glasses mr-2"></i>${engineer.title}</h3>
+      </div>
+      <div class="card-body"
+        <ul class="list-group">
           <li class="list-group-item id">Employee ID: ${engineer.id}</li>
-          <li class="list-group-item office">Github Username: ${engineer.github}</li>
-          <li class="list-group-item email">Employee Email: ${engineer.email}</li>
+          <li class="list-group-item office">Github Username: <a href="https://github.com/${engineer.github}" target="_blank" rel="noopener noreferrer">${engineer.github}</a></li>
+          <li class="list-group-item email">Employee Email: <a href="mailto:${engineer.email}">${engineer.email}</a></li>
         </ul>
       </div>
-      </div>
+    </div>
 `;
 }
 
 function generateInternCard(intern) {
   return `
-      <div class="card" style="width: 18rem">
-      <div class="card-body">
-        <h5 class="card-title">${intern.name}</h5>
-        <h6 class="card-subtitle mb-2 text-muted">${intern.title}</h6>
-        <ul class="list-group list-group-flush">
+    <div class="card employee-card" style="width: 18rem">
+      <div class="card-header bg-primary text-light">
+        <h2 class="card-title">${intern.name}</h2>
+        <h3 class="card-subtitle"><i class="fas fa-user-graduate mr-2"></i>${intern.title}</h3>
+      </div>
+      <div class="card-body"
+        <ul class="list-group">
           <li class="list-group-item id">Employee ID: ${intern.id}</li>
           <li class="list-group-item office">School: ${intern.school}</li>
-          <li class="list-group-item email">Employee Email: ${intern.email}</li>
+          <li class="list-group-item email">Employee Email: <a href="mailto:${intern.email}">${intern.email}</a></li>
         </ul>
       </div>
-      </div>
+    </div>
 `;
 }
 
